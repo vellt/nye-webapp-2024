42cdf93efbf332c23dd3df0dd4427b4401715442
Szanto Benjamin <szantobenjamin97@gmail.com>
2024-03-31 16:17:23 +0200

    tasks' solutions added

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,18 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  const sum: number = gameSet.reduce(pointCalculatorRPS, 0);
+  return sum;
 };
+
+const pointCalculatorRPS= (sum: number, input: RPSInput): number => {
+  const {outcome, shape} = input;
+  let shapePoints: number = Points.get(shape) ?? 0;
+  let outcomePoints: number = Points.get(outcome) ?? 0;
+
+  switch (outcome) {
+    case Outcome.DRAW: return sum + shapePoints + outcomePoints;
+    case Outcome.WIN: return ++sum + (shapePoints % 3) + outcomePoints;
+    case Outcome.LOOSE: return ++sum + (++shapePoints % 3) + outcomePoints;
+  }
+}
--- src/task-05/task-05.ts
+++ src/task-05/task-05.ts
@@ -2,5 +2,73 @@ import { Movie, Genre, SearchParams, SearchResults, OrderBy, Direction } from '.
 import { MovieService } from './services';
 
 export const searchMovies = async (params: SearchParams): Promise<SearchResults> => {
-  return Promise.reject('Not implemented');
+  try{
+    // alapértelmezések
+    const offset: number = params.offset ?? 0;
+    const limit: number = params.limit ?? 12;
+    const order: string = params.orderBy ?? 'title';
+    const direction: string = params.direction ?? 'ASC';
+    // adat lekérése
+    const MOVIES: Movie[] = await MovieService.getMovies();
+
+    // leszűrünk query-re
+    let byQuery: Movie[] = [];
+    if (params.query) {
+      const searchQuery: string = params.query.toLowerCase();
+      MOVIES.forEach((movie: Movie) => {
+        const { title, overview } = movie;
+        if (
+          title.toLowerCase().indexOf(searchQuery) > -1 ||
+          overview.toLowerCase().indexOf(searchQuery) > -1
+        ) {
+          byQuery.push(movie);
+        }
+      });
+    } else {
+      // ha nincs szűrés, az összes film
+      byQuery = MOVIES;
+    }
+  
+    // Genre
+    let results: Movie[] = [];
+    if (params.genre) {
+      byQuery.forEach((movie) => {
+        if (movie.genres) {
+          movie.genres.forEach((genre) => {
+            if (params.genre && params.genre.includes(genre)) { 
+              results.push(movie);
+            }
+          });
+        }
+      });
+    } else {
+      results = byQuery;
+    }
+  
+    let orderedList: Movie[] = results.sort((movie1, movie2) => {
+      let result = 0;
+      if ((order === 'title')) { // csak ezekre rendezhet
+        result = (movie1.title).localeCompare(movie2.title);
+      }
+      else if (order === 'release_date') {
+        result = movie1.release_date.getTime() - movie2.release_date.getTime();
+      } else if (order === 'vote_average') {
+        result = (movie1.vote_average ?? 0) - (movie2.vote_average ?? 0);
+      }
+      return direction === 'ASC' ? result : -result;
+    });
+    
+    // offset + limit
+    let finalList=orderedList.slice(offset, offset + limit);
+    return Promise.resolve({
+      total: !finalList.length ? finalList.length: orderedList.length,
+      movies: finalList
+    });
+
+  }catch(err){
+    return Promise.resolve({
+      total: 0,
+      movies: []
+    });
+  }
 };
